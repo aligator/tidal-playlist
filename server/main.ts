538dc897cd6203a8f34e829df8e1a0c9ad2bceac
aligator <me@aligator.dev>
2026-04-28 08:21:55 +0200

    Fix fonts loading

--- server/main.ts
+++ server/main.ts
@@ -1,5 +1,5 @@
 import { Application, Router, send } from '@oak/oak';
-import { assertServerConfig, PORT, WEB_DIST_DIR } from './config.ts';
+import { assertServerConfig, IS_DEV, PORT, WEB_DIST_DIR } from './config.ts';
 import { createAuthRouter } from './routes/auth.ts';
 
 assertServerConfig();
@@ -31,10 +31,10 @@ app.use(async (ctx, next) => {
     [
       "default-src 'self'",
       "script-src 'self'",
-      "style-src 'self' https://fonts.googleapis.com",
+      "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "font-src 'self' https://fonts.gstatic.com",
       "img-src 'self' data:",
-      "connect-src 'self' https://openapi.tidal.com https://auth.tidal.com https://login.tidal.com;",
+      "connect-src 'self' https://openapi.tidal.com https://auth.tidal.com https://login.tidal.com https://api.tidal.com;",
       "frame-ancestors 'none'",
       "object-src 'none'",
       "base-uri 'self'",
@@ -45,6 +45,11 @@ app.use(async (ctx, next) => {
   ctx.response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
   ctx.response.headers.set('X-Frame-Options', 'DENY');
   ctx.response.headers.set('Permissions-Policy', 'camera=(), microphone=(), geolocation=()');
+
+  // TODO: maybe remove it since that should be done by a proxy...
+  if (!IS_DEV) {
+    ctx.response.headers.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+  }
 });
 app.use(router.routes());
 app.use(router.allowedMethods());
